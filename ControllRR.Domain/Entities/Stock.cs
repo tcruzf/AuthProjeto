bc73063c6e731d466864e49d4fc2593c76f31544
tcruzf <thiago@thiagoferreira.net>
2025-01-28 18:28:32 -0300

    Relacionamento Stock

--- ControllRR.Domain/Entities/Stock.cs
+++ ControllRR.Domain/Entities/Stock.cs
@@ -13,5 +13,21 @@ public class Stock
 
     // Relacionamento com as movimentações
     public ICollection<StockManagement> Movements { get; set; } = new List<StockManagement>();
+
+    public Stock(){
+
+    }
+
+    public Stock(int id, string? productName, string? productDescription, int productQuantity, string? productApplication, string? productReference) 
+    {
+        Id = id;
+        ProductName = productName;
+        ProductDescription = productDescription;
+        ProductQuantity = productQuantity;
+        ProductApplication = productApplication;
+        ProductReference = productReference;
+
+    }
+
 }
 
--- ControllRR.Domain/Entities/StockManagement.cs
+++ ControllRR.Domain/Entities/StockManagement.cs
@@ -17,7 +17,28 @@ public class StockManagement
     public int Quantity { get; set; }
 
     // Relacionamento com o estoque
+    // O LinQ é inteligente o suficiente para entender que é um vinculo com a class Stock
+    // Referncia => https://learn.microsoft.com/en-us/ef/ef6/fundamentals/relationships
     public int StockId { get; set; }
     public Stock Stock { get; set; } = null!;
+
+    // Construtor vazio - Quando usar?
+    // Construtor vazio necessário para ORM ou deserialização
+    public StockManagement()
+    {
+
+    }
+    // No caso preciso impedir que seja repassado dados invalidos
+    // Existe uma necessidade de semear alguns dados no ambiente de desenvolvimento.
+    // Referencia =>
+    public StockManagement(int id, DateTime movementDate, StockMovementType movementType, int quantity, Stock stock)
+    {
+        Id = id;
+        MovementDate = movementDate;
+        MovementType = movementType;
+        Quantity = quantity;
+        Stock = stock;
+        
+    }
 
 }
