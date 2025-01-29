af78de1ba6a359bf65bd843988a351fa542caa47
tcruzf <thiago@thiagoferreira.net>
2025-01-29 16:16:45 -0300

    Alteração da classe base de usuarios para ApplicationUser

--- ControllRR.Domain/Entities/ApplicationUser.cs
+++ ControllRR.Domain/Entities/ApplicationUser.cs
@@ -11,8 +11,22 @@ public class ApplicationUser : IdentityUser
     public int Register { get; set; }
     public string? Role { get; set; }
 
+    public string? Phone { get; set; }
+
+    ICollection<Maintenance>? Maintenances{ get; set; } = new List<Maintenance>();
+
     public ApplicationUser()
     {
 
     }
+
+    public ApplicationUser(int id, string name, int register, string? role, string? phone)
+    {
+        Id = id;
+        Name = name;
+        Role = role;
+        Phone = phone;
+        //Maintenances = new List<Maintenance>();
+    }
+
 }
