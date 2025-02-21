d0d57d133533c36f7c49b51b4e998a681d3e813f
tcruzf <thiago@thiagoferreira.net>
2025-02-20 21:58:25 -0300

    V139

--- ControllRR.Presentation/wwwroot/js/site.js
+++ ControllRR.Presentation/wwwroot/js/site.js
@@ -2,16 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-document.addEventListener("DOMContentLoaded", function () {
-  var overlay = document.getElementById("overlay");
-  var content = document.getElementById("content");
-
-  // Aguarda tudo ser carregado (incluindo imagens)
-  window.onload = function () {
-      overlay.style.opacity = 0;
-      setTimeout(function () {
-          overlay.style.display = "none";
-          content.style.display = "block"; // Agora mostra o conteúdo
-      }, 500);
-  };
-});
+function displayBusyIndicator() {
+  document.getElementById("loading").style.display = "block";
+}
